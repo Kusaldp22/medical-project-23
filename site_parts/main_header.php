6d1506c01793ac8141325df1e5c2d7c9a594a05b
upekshaip <upekshaip@gmail.com>
2024-02-27 12:30:21 +0530

    fixed the headers

---
+++ site_parts/main_header.php
@@ -0,0 +1,43 @@
+<nav class="navbar bg-green">
+    <div class="container flex">
+        <a href="index.html" class="navbar-brand">
+            <img src="images/logo.png" alt="site logo" />
+        </a>
+        <button type="button" class="navbar-show-btn">
+            <img src="images/ham-menu-icon.png" />
+        </button>
+
+        <div class="navbar-collapse bg-white">
+            <button type="button" class="navbar-hide-btn">
+                <img src="images/close-icon.png" />
+            </button>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="our_process.php" class="nav-link">Our Process</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Help & Support</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Reviews</a>
+                </li>
+            </ul>
+            <div class="search-bar">
+                <form>
+                    <div class="search-bar-box flex">
+                        <span class="search-icon flex">
+                            <img src="images/search-icon-dark.png" />
+                        </span>
+                        <input type="search" class="search-control" placeholder="Search here" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
