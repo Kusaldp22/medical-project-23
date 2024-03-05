259110615773bcb570770b8a0ad07cc49dbee0e2
upekshaip <upekshaip@gmail.com>
2024-03-05 16:37:24 +0530

    added dash pages

---
+++ history.php
@@ -0,0 +1,7 @@
+<?php
+$title = "Patient History";
+$web_title = "Patient History";
+include_once "site_parts/dash_header.php";
+$json_path = "config/credentials.json";
+include("db.php");
+?>
--- site_parts/dash_header.php
+++ site_parts/dash_header.php
@@ -1,11 +1,10 @@
 <?php
-// session_start();
+session_start();
 
-// if (!isset($_SESSION["user"])) {
-//     header("Location: login.php");
-//     exit();
-// }
-
+if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
